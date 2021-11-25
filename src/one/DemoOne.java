c1a37284efbaa55bae04149ee2c8fe6b17d6cc07
vdaya <vdaya@LAPTOP-MUSS9J49.attlocal.net>
2021-11-24 20:02:26 -0500

    println added

--- src/one/DemoOne.java
+++ src/one/DemoOne.java
@@ -6,5 +6,9 @@ public class DemoOne {
 	public static void main(String[] args) {
 		
 		System.out.println("This is not private method");
+		
+		System.out.println("We are practicing GitHub");
 	}
+	
+	       
 }
